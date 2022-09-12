be07cf143fa2d7c5ba96840037dc4b1c7a966d73
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-09-12 18:14:32 +0300

    Initialize project

---
+++ recommendator/src/main/java/com/lampirg/recommendator/RecommendatorApplication.java
@@ -0,0 +1,13 @@
+package com.lampirg.recommendator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendatorApplication.class, args);
+	}
+
+}
---
+++ recommendator/src/test/java/com/lampirg/recommendator/RecommendatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lampirg.recommendator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
