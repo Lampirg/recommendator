d4f4aa41e2378382ffef06f22a21a10c54668cf3
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-11-10 18:26:18 +0300

    feature: new static converter from MAL node to AnimeTitle

--- recommendator/src/main/java/com/lampirg/recommendator/model/AnimeTitle.java
+++ recommendator/src/main/java/com/lampirg/recommendator/model/AnimeTitle.java
@@ -1,8 +1,13 @@
 package com.lampirg.recommendator.model;
 
+import com.lampirg.recommendator.mal.Node;
+
 public record AnimeTitle(
         long id,
         String name,
         String imageUrl
 ) {
+    public static AnimeTitle retreiveFromMalNode(Node node) {
+        return new AnimeTitle(node.id(), node.title(), node.mainPicture().getLargeIfPresent());
+    }
 }
