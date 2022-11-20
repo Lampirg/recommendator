132c2a40bb7df63b706026d99252f5fd70f1497d
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-11-21 00:01:52 +0300

    update: add TODO

--- recommendator/src/main/java/com/lampirg/recommendator/anidb/AnimeSiteCommunicator.java
+++ recommendator/src/main/java/com/lampirg/recommendator/anidb/AnimeSiteCommunicator.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.Set;
 
 public interface AnimeSiteCommunicator {
+    // TODO: add getters for different types of user lists (like dropped, on-hold, etc.), maybe in sub interface
     Set<UserAnimeTitle> getUserAnimeList(String username);
     Set<AnimeRecommendation> getSimilarAnimeTitles(Set<UserAnimeTitle> animeTitles);
 }
