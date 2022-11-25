39b8a09ca77de164eb99d2a21cb9064f22771e13
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-11-26 00:06:18 +0300

    refactor: extract TittleMapper as public interface with 2 implementations

--- recommendator/src/main/java/com/lampirg/recommendator/anidb/mal/MalCommunicator.java
+++ recommendator/src/main/java/com/lampirg/recommendator/anidb/mal/MalCommunicator.java
@@ -5,10 +5,12 @@ import com.lampirg.recommendator.anidb.mal.json.Data;
 import com.lampirg.recommendator.anidb.mal.json.Recommendation;
 import com.lampirg.recommendator.anidb.mal.json.queries.GetAnimeDetail;
 import com.lampirg.recommendator.anidb.mal.json.queries.GetUserListJsonResult;
+import com.lampirg.recommendator.anidb.mal.titlemapper.TitleMapper;
 import com.lampirg.recommendator.model.AnimeRecommendation;
 import com.lampirg.recommendator.model.AnimeTitle;
 import com.lampirg.recommendator.model.UserAnimeTitle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
@@ -26,8 +28,10 @@ import java.util.concurrent.*;
 @PropertySource("classpath:mal security code.yml")
 public class MalCommunicator implements AnimeSiteCommunicator {
 
-    @Autowired
+
     private RestTemplate restTemplate;
+
+    private TitleMapper titleMapper;
 
     @Value("${clientIdHeader}")
     private String clientIdHeader;
@@ -35,6 +39,16 @@ public class MalCommunicator implements AnimeSiteCommunicator {
     private String clientId;
     private HttpHeaders authHeader;
     HttpEntity<String> request;
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Autowired
+    public void setTitleMapper(@Qualifier("concurrent") TitleMapper titleMapper) {
+        this.titleMapper = titleMapper;
+    }
 
     @PostConstruct
     private void init() {
@@ -65,21 +79,15 @@ public class MalCommunicator implements AnimeSiteCommunicator {
 
     @Override
     public Set<AnimeRecommendation> getSimilarAnimeTitles(Set<UserAnimeTitle> animeTitles) {
-        TitleMapper titleMapper = new TitleMapper();
+        titleMapper.setRequest(request);
         titleMapper.fillToExclude(animeTitles);
         CompletableFuture<Void> future = CompletableFuture.allOf();
         for (UserAnimeTitle title : animeTitles) {
             future = CompletableFuture.allOf(future,
-                    CompletableFuture.runAsync(() -> {
-                        try {
-                            titleMapper.findAndAddTitleRecommendations(title);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }));
+                    CompletableFuture.runAsync(() -> titleMapper.findAndAddTitleRecommendations(title)));
         }
         future.join();
-        Map<AnimeTitle, Integer> result = new HashMap<>(titleMapper.recommendedAnime);
+        Map<AnimeTitle, Integer> result = new HashMap<>(titleMapper.getRecommendedAnimeMap());
         Set<AnimeRecommendation> animeRecommendationSet = new HashSet<>();
         result.forEach(
                 (key, value) -> animeRecommendationSet.add(new AnimeRecommendation(key, value))
@@ -87,49 +95,4 @@ public class MalCommunicator implements AnimeSiteCommunicator {
         return Set.copyOf(animeRecommendationSet);
     }
 
-    private class TitleMapper {
-        private final Map<AnimeTitle, Integer> recommendedAnime = new ConcurrentHashMap<>();
-        private Set<AnimeTitle> toExclude = Set.of();
-        DelayQueue<DelayedResponse> delayQueue = new DelayQueue<>();
-        private long startTime;
-
-        private void fillToExclude(Set<UserAnimeTitle> animeTitles) {
-            toExclude = new HashSet<>();
-            for (UserAnimeTitle title : animeTitles) {
-                toExclude.add(title.animeTitle());
-            }
-            toExclude = Set.copyOf(toExclude);
-        }
-
-        private void findAndAddTitleRecommendations(UserAnimeTitle title) throws InterruptedException {
-            String url = "https://api.myanimelist.net/v2/anime/"+title.animeTitle().id()+"?fields=recommendations";
-            delayQueue.add(new DelayedResponse(url));
-            ResponseEntity<GetAnimeDetail> response = restTemplate.exchange(delayQueue.take().url, HttpMethod.GET, request, GetAnimeDetail.class);
-            startTime = System.currentTimeMillis();
-            for (Recommendation recommendation : Objects.requireNonNull(response.getBody()).recommendations()) {
-                AnimeTitle animeTitle = AnimeTitle.retrieveFromMalNode(recommendation.node());
-                if (toExclude.contains(animeTitle))
-                    continue;
-                recommendedAnime.merge(animeTitle, title.score(), Integer::sum);
-            }
-        }
-
-        private class DelayedResponse implements Delayed {
-
-            private final String url;
-            private final static int DELAY = 500;
-            public DelayedResponse(String url) {
-                this.url = url;
-            }
-            @Override
-            public long getDelay(TimeUnit unit) {
-                long dif = startTime + DELAY - System.currentTimeMillis();
-                return unit.convert(dif, TimeUnit.MILLISECONDS);
-            }
-            @Override
-            public int compareTo(Delayed o) {
-                return 0;
-            }
-        }
-    }
 }
---
+++ recommendator/src/main/java/com/lampirg/recommendator/anidb/mal/titlemapper/ConcurrentTitleMapper.java
@@ -0,0 +1,94 @@
+package com.lampirg.recommendator.anidb.mal.titlemapper;
+
+import com.lampirg.recommendator.anidb.mal.json.Recommendation;
+import com.lampirg.recommendator.anidb.mal.json.queries.GetAnimeDetail;
+import com.lampirg.recommendator.model.AnimeTitle;
+import com.lampirg.recommendator.model.UserAnimeTitle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+@Component("concurrent")
+public class ConcurrentTitleMapper implements TitleMapper {
+
+    RestTemplate restTemplate;
+    HttpEntity<String> request;
+
+    private final Map<AnimeTitle, Integer> recommendedAnime = new ConcurrentHashMap<>();
+    private Set<AnimeTitle> toExclude = Set.of();
+    DelayQueue<DelayedResponse> delayQueue = new DelayQueue<>();
+    private long startTime;
+
+    @Override
+    public Map<AnimeTitle, Integer> getRecommendedAnimeMap() {
+        return recommendedAnime;
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public void setRequest(HttpEntity<String> request) {
+        this.request = request;
+    }
+
+    public void fillToExclude(Set<UserAnimeTitle> animeTitles) {
+        toExclude = new HashSet<>();
+        for (UserAnimeTitle title : animeTitles) {
+            toExclude.add(title.animeTitle());
+        }
+        toExclude = Set.copyOf(toExclude);
+    }
+
+    public void findAndAddTitleRecommendations(UserAnimeTitle title) {
+        String url = "https://api.myanimelist.net/v2/anime/"+title.animeTitle().id()+"?fields=recommendations";
+        delayQueue.add(new DelayedResponse(url));
+        ResponseEntity<GetAnimeDetail> response = null;
+        try {
+            response = restTemplate.exchange(
+                    delayQueue.take().url, HttpMethod.GET, request, GetAnimeDetail.class);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        startTime = System.currentTimeMillis();
+        for (Recommendation recommendation : Objects.requireNonNull(response.getBody()).recommendations()) {
+            AnimeTitle animeTitle = AnimeTitle.retrieveFromMalNode(recommendation.node());
+            if (toExclude.contains(animeTitle))
+                continue;
+            recommendedAnime.merge(animeTitle, title.score(), Integer::sum);
+        }
+    }
+
+    private class DelayedResponse implements Delayed {
+
+        private final String url;
+        private final static int DELAY = 500;
+        public DelayedResponse(String url) {
+            this.url = url;
+        }
+        @Override
+        public long getDelay(TimeUnit unit) {
+            long dif = startTime + DELAY - System.currentTimeMillis();
+            return unit.convert(dif, TimeUnit.MILLISECONDS);
+        }
+        @Override
+        public int compareTo(Delayed o) {
+            return 0;
+        }
+    }
+}
