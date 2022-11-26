e125d810eeb03e8f28fdfc8cc28fd1a274f940b2
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-11-26 16:43:31 +0300

    Fix: Test Class
    Now it operates with WebTestClient

--- recommendator/src/test/java/com/lampirg/recommendator/TestMalCommunicator.java
+++ recommendator/src/test/java/com/lampirg/recommendator/TestMalCommunicator.java
@@ -1,42 +1,35 @@
 package com.lampirg.recommendator;
 
 import com.lampirg.recommendator.anidb.AnimeSiteCommunicator;
-import com.lampirg.recommendator.model.AnimeRecommendation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.StopWatch;
 
-import java.util.Set;
-
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "PT90S")
 public class TestMalCommunicator {
-
-    private ApplicationContext context;
-    private AnimeSiteCommunicator singleThreadCommunicator;
-    private AnimeSiteCommunicator concurrentThreadCommunicator;
 
     private static Logger logger = LogManager.getLogger(TestMalCommunicator.class);
 
-    @Autowired
-    public void setSingleThreadCommunicator(@Qualifier("single") AnimeSiteCommunicator communicator) {
-        this.singleThreadCommunicator = communicator;
-    }
+    WebTestClient webTestClient;
 
     @Autowired
-    public void setConcurrentThreadCommunicator(@Qualifier("concurrent") AnimeSiteCommunicator communicator) {
-        this.concurrentThreadCommunicator = communicator;
+    public void setWebTestClient(WebTestClient webTestClient) {
+        this.webTestClient = webTestClient;
     }
 
-    private void getRecommendationForUser(AnimeSiteCommunicator communicator, String user, int jumps) {
+    private void getRecommendationForUser(String user, int jumps) {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         for (int i = 0; i < jumps; i++) {
-            communicator.getSimilarAnimeTitles(user);
+            webTestClient.get().uri("/recommend/user")
+                    .exchange().expectStatus().is2xxSuccessful();
         }
         stopWatch.stop();
         logger.info(stopWatch.getTotalTimeSeconds() / jumps + " seconds");
@@ -44,19 +37,16 @@ public class TestMalCommunicator {
 
     @Test
     void getRecommendationForRimisaki() {
-        getRecommendationForUser(singleThreadCommunicator, "Rimisaki", 1);
-        getRecommendationForUser(concurrentThreadCommunicator, "Rimisaki", 1);
+        getRecommendationForUser("Rimisaki", 3);
     }
 
     @Test
     void getRecommendationForLampirg() {
-        getRecommendationForUser(singleThreadCommunicator, "Lampirg", 1);
-        getRecommendationForUser(concurrentThreadCommunicator, "Lampirg", 1);
+        getRecommendationForUser("Lampirg", 3);
     }
 
     @Test
     void getRecommendationForZerity() {
-        getRecommendationForUser(singleThreadCommunicator, "Zerity", 1);
-        getRecommendationForUser(concurrentThreadCommunicator, "Zerity", 1);
+        getRecommendationForUser("Zerity", 3);
     }
 }
