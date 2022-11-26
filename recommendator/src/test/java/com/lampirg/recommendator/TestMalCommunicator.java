d26b709846850a3bb5e9b8c89a9ee459bf15a583
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-11-26 15:12:04 +0300

    Update TestMalCommunicator.java
    It is actually not working due to incorrect scope of injected dependencies

--- recommendator/src/test/java/com/lampirg/recommendator/TestMalCommunicator.java
+++ recommendator/src/test/java/com/lampirg/recommendator/TestMalCommunicator.java
@@ -1,33 +1,38 @@
 package com.lampirg.recommendator;
 
 import com.lampirg.recommendator.anidb.AnimeSiteCommunicator;
-import com.lampirg.recommendator.model.UserAnimeTitle;
+import com.lampirg.recommendator.model.AnimeRecommendation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.ApplicationContext;
 import org.springframework.util.StopWatch;
 
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 public class TestMalCommunicator {
 
-    private AnimeSiteCommunicator communicator;
+    private ApplicationContext context;
+    private AnimeSiteCommunicator singleThreadCommunicator;
+    private AnimeSiteCommunicator concurrentThreadCommunicator;
 
     private static Logger logger = LogManager.getLogger(TestMalCommunicator.class);
 
     @Autowired
-    public void setCommunicator(AnimeSiteCommunicator communicator) {
-        this.communicator = communicator;
+    public void setSingleThreadCommunicator(@Qualifier("single") AnimeSiteCommunicator communicator) {
+        this.singleThreadCommunicator = communicator;
     }
 
-    private void getRecommendationForUser(String user, int jumps) {
+    @Autowired
+    public void setConcurrentThreadCommunicator(@Qualifier("concurrent") AnimeSiteCommunicator communicator) {
+        this.concurrentThreadCommunicator = communicator;
+    }
+
+    private void getRecommendationForUser(AnimeSiteCommunicator communicator, String user, int jumps) {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         for (int i = 0; i < jumps; i++) {
@@ -39,16 +44,19 @@ public class TestMalCommunicator {
 
     @Test
     void getRecommendationForRimisaki() {
-        getRecommendationForUser("Rimisaki", 1);
+        getRecommendationForUser(singleThreadCommunicator, "Rimisaki", 1);
+        getRecommendationForUser(concurrentThreadCommunicator, "Rimisaki", 1);
     }
 
     @Test
     void getRecommendationForLampirg() {
-        getRecommendationForUser("Lampirg", 1);
+        getRecommendationForUser(singleThreadCommunicator, "Lampirg", 1);
+        getRecommendationForUser(concurrentThreadCommunicator, "Lampirg", 1);
     }
 
     @Test
     void getRecommendationForZerity() {
-        getRecommendationForUser("Zerity", 1);
+        getRecommendationForUser(singleThreadCommunicator, "Zerity", 1);
+        getRecommendationForUser(concurrentThreadCommunicator, "Zerity", 1);
     }
 }
