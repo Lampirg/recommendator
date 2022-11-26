af65abcf9d0b5f08c279d7ddca88a15624d657ea
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-11-26 23:47:05 +0300

    TODO: add other sites support (Shikimori, aniDB, etc.)

--- recommendator/src/main/java/com/lampirg/recommendator/anidb/AnimeSiteCommunicator.java
+++ recommendator/src/main/java/com/lampirg/recommendator/anidb/AnimeSiteCommunicator.java
@@ -5,5 +5,6 @@ import com.lampirg.recommendator.anidb.model.AnimeRecommendation;
 import java.util.Set;
 
 public interface AnimeSiteCommunicator {
+    // TODO: add other sites support (Shikimori, aniDB, etc.)
     Set<AnimeRecommendation> getSimilarAnimeTitles(String username);
 }
