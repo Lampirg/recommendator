f93ea36d22f071f1fd793a1d0d7d932f55ff6e32
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-12-03 16:35:18 +0300

    Update AnimeTitle.java

--- recommendator/src/main/java/com/lampirg/recommendator/anidb/model/AnimeTitle.java
+++ recommendator/src/main/java/com/lampirg/recommendator/anidb/model/AnimeTitle.java
@@ -1,6 +1,7 @@
 package com.lampirg.recommendator.anidb.model;
 
 import com.lampirg.recommendator.anidb.mal.json.MalNode;
+import com.lampirg.recommendator.anidb.shikimori.json.ShikiNode;
 import com.lampirg.recommendator.anidb.shikimori.json.ShikiUserNode;
 
 public record AnimeTitle(
@@ -12,7 +13,7 @@ public record AnimeTitle(
         return new AnimeTitle(node.id(), node.title(), node.mainPicture().getLargeIfPresent());
     }
 
-    public static AnimeTitle retrieveFromShikiNode(ShikiUserNode node) {
-        return new AnimeTitle(node.anime().id(), node.anime().name(), node.anime().image().original());
+    public static AnimeTitle retrieveFromShikiNode(ShikiNode node) {
+        return new AnimeTitle(node.id(), node.name(), node.image().original());
     }
 }
