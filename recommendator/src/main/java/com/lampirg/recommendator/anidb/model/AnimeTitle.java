8cd51256a39a385f1bd1ee2311ce8578279c7b06
Lampirg <63813365+Lampirg@users.noreply.github.com>
2022-12-03 15:21:19 +0300

    Add retrieveFromShikiNode method

--- recommendator/src/main/java/com/lampirg/recommendator/anidb/model/AnimeTitle.java
+++ recommendator/src/main/java/com/lampirg/recommendator/anidb/model/AnimeTitle.java
@@ -1,13 +1,18 @@
 package com.lampirg.recommendator.anidb.model;
 
-import com.lampirg.recommendator.anidb.mal.json.Node;
+import com.lampirg.recommendator.anidb.mal.json.MalNode;
+import com.lampirg.recommendator.anidb.shikimori.json.ShikiNode;
 
 public record AnimeTitle(
         long id,
         String name,
         String imageUrl
 ) {
-    public static AnimeTitle retrieveFromMalNode(Node node) {
+    public static AnimeTitle retrieveFromMalNode(MalNode node) {
         return new AnimeTitle(node.id(), node.title(), node.mainPicture().getLargeIfPresent());
+    }
+
+    public static AnimeTitle retrieveFromShikiNode(ShikiNode node) {
+        return new AnimeTitle(node.anime().id(), node.anime().name(), node.anime().image().original());
     }
 }
