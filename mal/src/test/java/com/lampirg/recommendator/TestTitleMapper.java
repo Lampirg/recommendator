a060ede8915718fa98925b03ac49bd1762eec216
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-05 09:22:32 +0300

    Create TestTitleMapper.java

---
+++ mal/src/test/java/com/lampirg/recommendator/TestTitleMapper.java
@@ -0,0 +1,96 @@
+package com.lampirg.recommendator;
+
+import com.lampirg.recommendator.anidb.MalCacher;
+import com.lampirg.recommendator.anidb.titlemapper.ConcurrentTitleMapper;
+import com.lampirg.recommendator.anidb.titlemapper.SingleThreadTitleMapper;
+import com.lampirg.recommendator.anidb.titles.model.AnimeTitle;
+import com.lampirg.recommendator.anidb.titles.model.UserAnimeTitle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@ExtendWith(MockitoExtension.class)
+public class TestTitleMapper {
+
+    @Mock
+    MalCacher malCacher;
+    @InjectMocks
+    SingleThreadTitleMapper singleThreadTitleMapper;
+    @InjectMocks
+    ConcurrentTitleMapper concurrentTitleMapper;
+
+    private final List<AnimeTitle> titles = List.of(
+            new AnimeTitle(1, "Hadaske", "/notfound"),
+            new AnimeTitle(2, "Hadaske: Return to Omsk", "/totally-notfound"),
+            new AnimeTitle(3, "DeHadaske", "/no"),
+            new AnimeTitle(4, "Hadaske - Kukic", "/notfound")
+    );
+
+    private final Map<AnimeTitle, Set<AnimeTitle>> reccommendations = Map.ofEntries(
+            Map.entry(titles.get(0),
+                    Set.of(
+                            titles.get(2),
+                            titles.get(3)
+                    )),
+            Map.entry(titles.get(1), Set.of(titles.get(2)))
+    );
+
+    private final Set<UserAnimeTitle> userTitles = Set.of(
+            new UserAnimeTitle(titles.get(0), 10),
+            new UserAnimeTitle(titles.get(1), 1)
+    );
+    private final Set<UserAnimeTitle> toExclude = Set.of(new UserAnimeTitle(titles.get(2), 5));
+
+    @Test
+    void notGivenToExclude() {
+        Mockito.when(malCacher.getRecommendations(Mockito.any()))
+                .then(onMock -> reccommendations.get((AnimeTitle) onMock.getArgument(0)));
+        singleThreadTitleMapper.fillToExclude(Set.of());
+        var result = singleThreadTitleMapper.getRecommendedAnimeMap(userTitles);
+        Assertions.assertEquals(11, result.get(titles.get(2)));
+        Assertions.assertEquals(10, result.get(titles.get(3)));
+        concurrentTitleMapper.fillToExclude(Set.of());
+        result = concurrentTitleMapper.getRecommendedAnimeMap(userTitles);
+        Assertions.assertEquals(11, result.get(titles.get(2)));
+        Assertions.assertEquals(10, result.get(titles.get(3)));
+    }
+
+    @Test
+    void givenToExclude() {
+        Mockito.when(malCacher.getRecommendations(Mockito.any()))
+                .then(onMock -> reccommendations.get((AnimeTitle) onMock.getArgument(0)));
+        singleThreadTitleMapper.fillToExclude(toExclude);
+        var result = singleThreadTitleMapper.getRecommendedAnimeMap(userTitles);
+        Assertions.assertNull(result.get(titles.get(2)));
+        Assertions.assertEquals(10, result.get(titles.get(3)));
+        concurrentTitleMapper.fillToExclude(toExclude);
+        result = concurrentTitleMapper.getRecommendedAnimeMap(userTitles);
+        Assertions.assertNull(result.get(titles.get(2)));
+        Assertions.assertEquals(10, result.get(titles.get(3)));
+    }
+
+    @Test
+    void whenSecondTime() {
+        Mockito.when(malCacher.getRecommendations(Mockito.any()))
+                .then(onMock -> reccommendations.get((AnimeTitle) onMock.getArgument(0)));
+        singleThreadTitleMapper.fillToExclude(Set.of());
+        var result = singleThreadTitleMapper.getRecommendedAnimeMap(userTitles);
+        Assertions.assertEquals(result, Assertions.assertTimeoutPreemptively(Duration.ofMillis(1), () -> singleThreadTitleMapper.getRecommendedAnimeMap(userTitles)));
+        Assertions.assertEquals(11, result.get(titles.get(2)));
+        Assertions.assertEquals(10, result.get(titles.get(3)));
+        concurrentTitleMapper.fillToExclude(Set.of());
+        result = concurrentTitleMapper.getRecommendedAnimeMap(userTitles);
+        Assertions.assertEquals(result, Assertions.assertTimeoutPreemptively(Duration.ofMillis(1), () -> concurrentTitleMapper.getRecommendedAnimeMap(userTitles)));
+        Assertions.assertEquals(11, result.get(titles.get(2)));
+        Assertions.assertEquals(10, result.get(titles.get(3)));
+    }
+}
