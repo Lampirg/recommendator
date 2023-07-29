2a219cbe87e81dd97bbb62e05941bedbc7d87c22
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-07-29 19:51:44 +0300

    Create RestTemplateProvider

--- frontend/src/main/java/com/lampirg/frontend/FrontendApplication.java
+++ frontend/src/main/java/com/lampirg/frontend/FrontendApplication.java
@@ -2,8 +2,14 @@ package com.lampirg.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FrontendApplication {
 
 	public static void main(String[] args) {
